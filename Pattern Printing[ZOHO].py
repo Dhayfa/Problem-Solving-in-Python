c56602d7b9b2629c3c0dd5a77db04237d2ab9646
Dhayfa <66622185+Dhayfa@users.noreply.github.com>
2022-12-22 22:10:40 +0530

    Update Pattern Printing[ZOHO].py

--- Pattern Printing[ZOHO].py
+++ Pattern Printing[ZOHO].py
@@ -1,18 +1,21 @@
-S = list('tiger')
+S = list(input().strip()) #gets input
 new_S = []
-Space = (len(S) -2)
-mid = S[(len(S)//2)]
+Space = (len(S) -2) 
+mid = S[(len(S)//2)] #finds the mid value and seprates it
 
-for i in range(len(S)//2 +1): 
+for i in range(len(S)//2 +1): #creates a list with proper spacing in between words and segregates words
     if Space < 0:
         break
     new_S.append(S[i]+" "*Space+S[-i-1])
     Space -= 2
 
-final_S = []
-for i , j in enumerate(new_S):
+final_S = [] #saves the output so it could reverse it later
+
+for i , j in enumerate(new_S): #prints the first half of output
     final_S.append(" "*i+j)
     print(' '*i +j)
-print(" "*(len(S)//2)+mid)
-for i in final_S[::-1]:
+    
+print(" "*(len(S)//2)+mid) #print the mid value
+
+for i in final_S[::-1]: #prints the second half of the input which is just reverse of the first half
     print(i)
